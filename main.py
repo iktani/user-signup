31f2d2ef79939d7a838c978e29fc0c0f9b5b918c
Irwin Tani <itani05@gmail.com>
2018-09-21 20:33:52 -0500

    Testing search for spaces in user inputs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, redirect, render_template
+import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -28,17 +29,24 @@ def validate():
         
     # check length requirement for username and password >3 <20
 
-    if len(username) < 3 or len(username) > 20 or username == username_error:
+    if len(username) < 3 or len(username) > 20 or username == username_error or not re.search("",username):
         username_error = "Username entered is not valid. **Must have minimum length of 3 characters and maximum length of 20 characters"
 
-    if len(password) < 3 or len(password) > 20 or password == password_error:
+    if len(password) < 3 or len(password) > 20 or password == password_error or not re.search("",password):
         password_error = "Password enterd is not valid. **Must have minimum length of 3 characters and maximum length of 20 characters"
         password = ""
         verify_password = ""
 
     # make sure there are no spaces in username or password
 
-    if         
+    
+
+    # if all fields pass validation, redirect to welcome page
+
+    if not username_error and not password_error and not verify_password_error and not email_error:
+        return render_template("welcome.html", username=username)  
+    else:
+
 
 
 
