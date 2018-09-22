e4cd0df8b625a2cdd82a740a89b5ab5afaa96424
Irwin Tani <itani05@gmail.com>
2018-09-21 23:43:51 -0500

    Working app. No regex yet.

--- main.py
+++ main.py
@@ -12,19 +12,19 @@ def index():
 
 
 def is_a_valid_email(email):
-    if len(email) >= 3 and len(email) <= 20 and email.count(@) == 1 and email.count(.) == 1:
+    if not re.search(" ",email) and len(email) >= 3 and len(email) <= 20 and email.count("@") == 1 and email.count(".") == 1:
         return True
     else:
         return False    
 
 # validate inputs upon submission
-@app.route("/validate-inputs", methods=['POST'])
+@app.route("/", methods=['POST'])
 def validate():
     # get user inputs
-    username = form.request["username"]
-    password = form.request["password"]
-    verify_password = form.request["verify_password"]
-    email = form.request["email"]
+    username = request.form["username"]
+    password = request.form["password"]
+    verify_password = request.form["verify_password"]
+    email = request.form["email"]
 
     # create variables for error messages, set to ""
 
@@ -37,19 +37,15 @@ def validate():
     # validate user inputs
 
     if len(username) < 3 or len(username) > 20 or not username or re.search(" ",username):
-        username_error = "Username entered is not valid. **Must have minimum length of 3 characters and maximum length of 20 characters"
+        username_error = "Username entered is not valid. **Must have minimum length of 3 characters and maximum length of 20 characters with no spaces."
 
     if len(password) < 3 or len(password) > 20 or not password or re.search(" ",password):
-        password_error = "Password entered is not valid. **Must have minimum length of 3 characters and maximum length of 20 characters"
-        password = ""
-        verify_password = ""
+        password_error = "Password entered is not valid. **Must have minimum length of 3 characters and maximum length of 20 characters with no spaces."
 
     # if passwords do not match, show different error
 
     if password != verify_password:
         verify_password_error = "Entered passwords do not match. Passwords must match. Please re-enter."
-        password = ""
-        verify_password = ""
 
     # validate email if field is not empty
 
@@ -57,11 +53,15 @@ def validate():
         email_error = email_error
     else:
         if not is_a_valid_email(email):
-            email_error = "Email entered is not a valid format. Please re-enter"     
-        
-
+            email_error = "Email entered is not a valid format. Please re-enter"
+            
+    # if username or email are errors, erase password and verify_password fields
     
-
+    if username_error or email_error or password_error or verify_password_error:
+        password = ""
+        verify_password = ""              
+     
+    
     # if all fields pass validation, redirect to welcome page, if not return error messages
 
     if not username_error and not password_error and not verify_password_error and not email_error:
